9f5c5dcd4fc642866cb3b8590c461a264b717de7
Ragul Kanthasamy <k1903718@kingston.ac.uk>
2022-03-01 08:05:54 +0000

    Added Delete and Edit for Tracking

--- index.js
+++ index.js
@@ -105,6 +105,36 @@ app.get('/tracking', (req, res) => {
     })
 })
 
+//Delete Tracking
+app.delete("/deletetracking/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM tracking WHERE id = ?", id, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+//Edit Tracking
+
+app.put("/updatetracking", (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query(
+      "UPDATE employees SET wage = ? WHERE id = ?",
+      [wage, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+
 app.listen(3001, () => {
     console.log('Server Running')
 })
